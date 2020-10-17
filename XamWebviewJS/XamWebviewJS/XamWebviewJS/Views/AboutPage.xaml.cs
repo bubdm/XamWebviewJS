82d8cd062d3db40f8523774aa90dc8132afafdea
Faruk Abdo Feres <faruk_feres@hotmail.com>
2020-10-17 14:25:29 -0300

    Initial Commit

---
+++ XamWebviewJS/XamWebviewJS/XamWebviewJS/Views/AboutPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.ComponentModel;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XamWebviewJS.Views
+{
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            webView.Source = "https://www.google.com";
+           
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                await Task.Delay(500);
+
+                await webView.EvaluateJavaScriptAsync("document.querySelectorAll('#gb-main header')[0].innerHTML = '<h2>XAMARIN</h2>'; ");
+                await webView.EvaluateJavaScriptAsync("document.querySelectorAll('#gb-main header')[0].style.padding = '10px'; ");
+                await webView.EvaluateJavaScriptAsync("document.querySelectorAll('#gb-main header')[0].style.color = 'red'; ");
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Error", ex.Message, "OK");
+            }
+        }
+    }
+}
---
+++ XamWebviewJS/XamWebviewJS/XamWebviewJS/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+using XamWebviewJS.ViewModels;
+
+namespace XamWebviewJS.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
